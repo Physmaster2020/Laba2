847054e7619e6a8584773daf51769476d990bf74
Vitaliy <physmasterPM@yandex.ru>
2020-05-19 22:37:50 +0300

    Added entry point

---
+++ src/po83/borisov/oop/model/Exercise.java
@@ -0,0 +1,9 @@
+package po83.borisov.oop.model;
+
+public interface Exercise {
+
+    String getName();
+    void setName(String name);
+    int getDuration();
+    void setDuration(int duration);
+}
